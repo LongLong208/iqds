7df3d28c489fe6405c2720897e32791b309c41dc
LongLong208 <1060623018@qq.com>
2021-11-26 23:02:23 +0800

    Create questions.sql

---
+++ src/main/sql/questions.sql
@@ -0,0 +1,7 @@
+use iqds;
+insert into knowledge(knowledgeName) values("常识");
+insert into question(title, questionType, knowledgeId) values("社会主义法实施的基本要求是（）", 0, 1);
+insert into answerItem(title, isTrue, questionId) values("司法机关严格适用法律", 0, 1);
+insert into answerItem(title, isTrue, questionId) values("国家各类机关依法办事", 0, 1);
+insert into answerItem(title, isTrue, questionId) values("各级干部模范带头守法", 0, 1);
+insert into answerItem(title, isTrue, questionId) values("全体公民和一切组织自觉守法", 1, 1);
